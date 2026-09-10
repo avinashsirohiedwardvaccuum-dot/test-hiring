9b671ee9b31c17255024da64d115f2f5d3a38faf
avinashsirohiedwardvaccuum-dot <avinashsirohi.edwardvaccuum@gmail.com>
2026-09-10 14:03:44 +0530

    Create Exception handlimg .js

---
+++ TaskListApi/Models/DTOs — TaskListApi/Models/Exception handlimg .js
@@ -0,0 +1,15 @@
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler(errorApp =>
+    {
+        errorApp.Run(async context =>
+        {
+            context.Response.StatusCode = 500;
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsJsonAsync(new
+            {
+                error = "An unexpected error occurred."
+            });
+        });
+    });
+
